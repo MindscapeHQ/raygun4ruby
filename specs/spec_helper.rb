80a848b5f5736b567494e5da3708a544bdbcfb3f
Taylor Lodge <ubermouse894@gmail.com>
2018-02-19 13:52:02 +1300

    Add FakeLogger to specs helper

--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -6,5 +6,25 @@ require "fakeweb"
 require "timecop"
 require "mocha/mini_test"
 require "webmock/minitest"
+require 'stringio'
 
 alias :context :describe
+
+class FakeLogger
+  def initialize
+    @logger = StringIO.new
+  end
+
+  def info(message)
+    @logger.write(message)
+  end
+
+  def reset
+    @logger.string = ""
+  end
+
+  def get
+    @logger.string
+  end
+end
+

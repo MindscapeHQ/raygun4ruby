11bb82ac150d7e330fd222bdfc79ad80ea68b280
Taylor Lodge <ubermouse894@gmail.com>
2018-02-19 15:28:24 +1300

    Return response from track_exception_sync

--- lib/raygun.rb
+++ lib/raygun.rb
@@ -131,8 +131,10 @@ module Raygun
     def track_exception_sync(exception_instance, env, user, retry_count)
       if should_report?(exception_instance)
         log('attempting to send exception')
-        Client.new.track_exception(exception_instance, env, user)
+        resp = Client.new.track_exception(exception_instance, env, user)
         log('sent payload to api')
+
+        resp
       end
     rescue Exception => e
       log('error sending exception to raygun, see failsafe logger for more information')

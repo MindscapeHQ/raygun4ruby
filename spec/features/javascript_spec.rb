2befef6f064fdcd9d14a4b37f379de1c5fd3397d
Mike Rogers <me@mikerogers.io>
2018-09-23 15:57:23 +0100

    Making sure non HTML files don't get the HTML injected

--- spec/features/javascript_spec.rb
+++ spec/features/javascript_spec.rb
@@ -9,7 +9,6 @@ feature 'JavaScript Tracking', feature: true do
   end
 
   it "No JS snippet is injected" do
-    expect(Raygun.configuration.js_api_key).to eq(nil)
     visit root_path
 
     expect(page.html).to_not include("<script type=\"text/javascript\">!function(a,b,c,d,e,f,g,h){a.RaygunObject=e,a[e]=a[e]||function(){(a[e].o=a[e].o||[]).push(arguments)},f=b.createElement(c),g=b.getElementsByTagName(c)[0],f.async=1,f.src=d,g.parentNode.insertBefore(f,g),h=a.onerror,a.onerror=function(b,c,d,f,g){h&&h(b,c,d,f,g),g||(g=new Error(b)),a[e].q=a[e].q||[],a[e].q.push({e:g})}}(window,document,\"script\",\"//cdn.raygun.io/raygun4js/raygun.min.js\",\"rg4js\");</script></head>")
@@ -19,12 +18,18 @@ feature 'JavaScript Tracking', feature: true do
   context 'With a JS API Key' do
     before { Raygun.configuration.js_api_key = 'Sample key' }
 
-    it "JS snippet is injected" do
-      expect(Raygun.configuration.js_api_key).to eq('Sample key')
+    it "JS snippet is injected requesting a HTML file" do
       visit root_path
 
       expect(page.html).to include("<script type=\"text/javascript\">!function(a,b,c,d,e,f,g,h){a.RaygunObject=e,a[e]=a[e]||function(){(a[e].o=a[e].o||[]).push(arguments)},f=b.createElement(c),g=b.getElementsByTagName(c)[0],f.async=1,f.src=d,g.parentNode.insertBefore(f,g),h=a.onerror,a.onerror=function(b,c,d,f,g){h&&h(b,c,d,f,g),g||(g=new Error(b)),a[e].q=a[e].q||[],a[e].q.push({e:g})}}(window,document,\"script\",\"//cdn.raygun.io/raygun4js/raygun.min.js\",\"rg4js\");</script></head>")
       expect(page.html).to include("<script type=\"text/javascript\">rg4js('apiKey', 'Sample key');rg4js('enableCrashReporting', true);</script></body>")
+    end
+
+    it "JS snippet is not injected requesting a JSON file" do
+      visit root_path(format: :json)
+
+      expect(page.html).to_not include("<script type=\"text/javascript\">!function(a,b,c,d,e,f,g,h){a.RaygunObject=e,a[e]=a[e]||function(){(a[e].o=a[e].o||[]).push(arguments)},f=b.createElement(c),g=b.getElementsByTagName(c)[0],f.async=1,f.src=d,g.parentNode.insertBefore(f,g),h=a.onerror,a.onerror=function(b,c,d,f,g){h&&h(b,c,d,f,g),g||(g=new Error(b)),a[e].q=a[e].q||[],a[e].q.push({e:g})}}(window,document,\"script\",\"//cdn.raygun.io/raygun4js/raygun.min.js\",\"rg4js\");</script></head>")
+      expect(page.html).to_not include("<script type=\"text/javascript\">rg4js('apiKey', '');rg4js('enableCrashReporting', true);</script></body>")
     end
   end
 end

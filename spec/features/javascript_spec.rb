a25f041c575a02e215aa74aa8bc21c7237e2fa89
Mike Rogers <me@mikerogers.io>
2018-12-26 21:06:50 +0000

    Making specs have a simpler include assertion

--- spec/features/javascript_spec.rb
+++ spec/features/javascript_spec.rb
@@ -11,8 +11,7 @@ feature 'JavaScript Tracking', feature: true do
   it "Does not inject the JS snippet" do
     visit root_path
 
-    expect(page.html).to_not include("<script type=\"text/javascript\">!function(a,b,c,d,e,f,g,h){a.RaygunObject=e,a[e]=a[e]||function(){(a[e].o=a[e].o||[]).push(arguments)},f=b.createElement(c),g=b.getElementsByTagName(c)[0],f.async=1,f.src=d,g.parentNode.insertBefore(f,g),h=a.onerror,a.onerror=function(b,c,d,f,g){h&&h(b,c,d,f,g),g||(g=new Error(b)),a[e].q=a[e].q||[],a[e].q.push({e:g})}}(window,document,\"script\",\"//cdn.raygun.io/raygun4js/raygun.min.js\",\"rg4js\");</script></head>")
-    expect(page.html).to_not include("<script type=\"text/javascript\">rg4js('apiKey', '');rg4js('enableCrashReporting', true);</script></body>")
+    expect(page.html).to_not include('cdn.raygun.io/raygun4js/1.14.0/raygun.min.js')
   end
 
   context 'With a JS API Key' do
@@ -21,15 +20,13 @@ feature 'JavaScript Tracking', feature: true do
     it "Injects the JS snippet" do
       visit root_path
 
-      expect(page.html).to include("<script type=\"text/javascript\">!function(a,b,c,d,e,f,g,h){a.RaygunObject=e,a[e]=a[e]||function(){(a[e].o=a[e].o||[]).push(arguments)},f=b.createElement(c),g=b.getElementsByTagName(c)[0],f.async=1,f.src=d,g.parentNode.insertBefore(f,g),h=a.onerror,a.onerror=function(b,c,d,f,g){h&&h(b,c,d,f,g),g||(g=new Error(b)),a[e].q=a[e].q||[],a[e].q.push({e:g})}}(window,document,\"script\",\"//cdn.raygun.io/raygun4js/raygun.min.js\",\"rg4js\");</script></head>")
-      expect(page.html).to include("<script type=\"text/javascript\">rg4js('apiKey', 'Sample key');rg4js('enableCrashReporting', true);</script></body>")
+      expect(page.html).to include('cdn.raygun.io/raygun4js/1.14.0/raygun.min.js')
     end
 
     it "Does not inject the JS snippet" do
       visit root_path(format: :json)
 
-      expect(page.html).to_not include("<script type=\"text/javascript\">!function(a,b,c,d,e,f,g,h){a.RaygunObject=e,a[e]=a[e]||function(){(a[e].o=a[e].o||[]).push(arguments)},f=b.createElement(c),g=b.getElementsByTagName(c)[0],f.async=1,f.src=d,g.parentNode.insertBefore(f,g),h=a.onerror,a.onerror=function(b,c,d,f,g){h&&h(b,c,d,f,g),g||(g=new Error(b)),a[e].q=a[e].q||[],a[e].q.push({e:g})}}(window,document,\"script\",\"//cdn.raygun.io/raygun4js/raygun.min.js\",\"rg4js\");</script></head>")
-      expect(page.html).to_not include("<script type=\"text/javascript\">rg4js('apiKey', '');rg4js('enableCrashReporting', true);</script></body>")
+      expect(page.html).to_not include('cdn.raygun.io/raygun4js/1.14.0/raygun.min.js')
     end
   end
 end

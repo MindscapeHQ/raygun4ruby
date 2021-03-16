1265cc9d95a0679fcde06ddaca99c9875654d41f
Samuel Holt <sam@raygun.io>
2021-03-17 10:07:44 +1300

    Rename Testable to RaygunTestable

--- lib/raygun/raygun_testable.rb
+++ lib/raygun/raygun_testable.rb
@@ -2,7 +2,7 @@ module Raygun
 
   class ItWorksException < StandardError; end
 
-  module Testable
+  module RaygunTestable
 
     def track_test_exception
       Raygun.configuration.silence_reporting = false

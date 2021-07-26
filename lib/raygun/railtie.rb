99fed6bc3cea8187220311ecda8618f3b78ed0f2
Roger Nesbitt <roger@raygun.com>
2021-07-26 17:11:22 +1200

    Wait until ActionDispatchRequest loaded only on Rails 7
    Ensure compatibility back to Rails 3 with this patch that supports Rails
    7 having a different load order.

--- lib/raygun/railtie.rb
+++ lib/raygun/railtie.rb
@@ -1,31 +1,39 @@
 require "raygun/middleware/rails_insert_affected_user"
 
 class Raygun::Railtie < Rails::Railtie
-  initializer "raygun.configure_rails_initialization" do |app|
-    ActiveSupport.on_load(:action_dispatch_request, run_once: true) do
-      # Thanks Airbrake: See https://github.com/rails/rails/pull/8624
-      middleware = if defined?(ActionDispatch::DebugExceptions)
-                    if Rails::VERSION::STRING >= "5"
-                      ActionDispatch::DebugExceptions
-                    else
-                      # Rails >= 3.2.0
-                      "ActionDispatch::DebugExceptions"
-                    end
-                  else
-                    # Rails < 3.2.0
-                    "ActionDispatch::ShowExceptions"
-                  end
+  def self.raygun_initialization(app)
+    # Thanks Airbrake: See https://github.com/rails/rails/pull/8624
+    middleware = if defined?(ActionDispatch::DebugExceptions)
+                   if Rails::VERSION::MAJOR >= 5
+                     ActionDispatch::DebugExceptions
+                   else
+                     # Rails >= 3.2.0
+                     "ActionDispatch::DebugExceptions"
+                   end
+                 else
+                   # Rails < 3.2.0
+                   "ActionDispatch::ShowExceptions"
+                 end
 
-      raygun_middleware = [
-        Raygun::Middleware::RailsInsertAffectedUser,
-        Raygun::Middleware::RackExceptionInterceptor,
-        Raygun::Middleware::BreadcrumbsStoreInitializer,
-        Raygun::Middleware::JavascriptExceptionTracking
-      ]
-      raygun_middleware = raygun_middleware.map(&:to_s) unless Rails::VERSION::STRING >= "5"
-      raygun_middleware.each do |m|
-        app.config.middleware.insert_after(middleware, m)
+    raygun_middleware = [
+      Raygun::Middleware::RailsInsertAffectedUser,
+      Raygun::Middleware::RackExceptionInterceptor,
+      Raygun::Middleware::BreadcrumbsStoreInitializer,
+      Raygun::Middleware::JavascriptExceptionTracking
+    ]
+    raygun_middleware = raygun_middleware.map(&:to_s) unless Rails::VERSION::MAJOR >= 5
+    raygun_middleware.each do |m|
+      app.config.middleware.insert_after(middleware, m)
+    end
+  end
+
+  initializer "raygun.configure_rails_initialization" do |app|
+    if Rails::VERSION::MAJOR >= 7
+      ActiveSupport.on_load(:action_dispatch_request, run_once: true) do
+        Raygun::Railtie.raygun_initialization(app)
       end
+    else
+      Raygun::Railtie.raygun_initialization(app)
     end
   end
 
